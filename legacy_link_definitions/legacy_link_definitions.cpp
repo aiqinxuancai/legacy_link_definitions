f63091e0f5ccb7a47baed4b00427836a766e5713
aiqinxuancai <aiqinxuancai@163.com>
2023-10-10 16:29:46 +0800

    添加项目文件。

---
+++ legacy_link_definitions/legacy_link_definitions.cpp
@@ -0,0 +1,28 @@
+// legacy_link_definitions.cpp : 定义静态库的函数。
+//
+
+#include "pch.h"
+#include "framework.h"
+#define _CRT_SECURE_NO_WARNINGS
+#include <time.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+    long compute_timezone_value() {
+        long t = 0;
+        _get_timezone(&t);
+        return t; // 示例值
+    }
+
+    long& timezone() {
+        static long value = compute_timezone_value();
+        return value;
+    }
+
+
+#ifdef __cplusplus
+}
+#endif
